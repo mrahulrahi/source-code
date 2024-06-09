494e0254ae9e1b9f85fef755c0d26f20ded4eb68
mrahulrahi <mrahulrahi@gmail.com>
2024-06-10 01:40:55 +0530

    4 page half completed

--- gu/src/pages/iqac/iqac.jsx
+++ gu/src/pages/iqac/iqac.jsx
@@ -24,7 +24,7 @@ const IQAC = () => {
           </div>
         </div>
       </div>
-      <div className="content-container pt-0">
+      <div className="content-container pt-0 text-container">
         <div className="container">
           <div className="row">
             <div className="col-lg-9 mx-auto">
@@ -33,6 +33,61 @@ const IQAC = () => {
                 <li>To ensure continuous improvement in the entire operations of the University.</li>
                 <li>To develop a quality system for conscious, consistent and programmed action to improve the academic and administrative performance of the University.</li>
                 <li>To promote measures for driving university's functioning towards quality enhancement and institutionalization of best practices.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content-container text-container light-bg-text-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-9 mx-auto">
+              <div className="heading text-center">
+                <h4>NISP Committee</h4>
+              </div>
+
+              <ul>
+                <li>Organized an expert talk on "CROWDED CAFES EMPTY LIBRARIES" for students and faculties on 22-04-2022.</li>
+                <li>Organized Seminar on "Demerger: A Strategy for Value Unlocking" for MBA students on 21-04-2022.</li>
+                <li>Organized Seminar on "Assessment of Service Quality towards Online Organised Retailers" for MBA students on 19-04-2022.</li>
+                <li>Organized Two Day Seminar on "Recent Advance Trends in Geotechnical Engineering” for civil engineering students on 11-04-22 and12-04-2022.</li>
+                <li>Organized a Motivational talk on "Shift in the Conscious States of Mind" for students and faculties on 04-04-2022.</li>
+                <li>Organized Seminar on "Forex Exposure and Managing Risk" for MBA students on 31-03-2022.</li>
+                <li>Organized Seminar on "Brand Elements and Importance of Leveraging" for MBA students on 29-03-2022.</li>
+                <li>Organized an expert Talk on "Essential Skills required for Civil Engineer" for students on 05-03-2022.</li>
+                <li>Organized Six days online Joint GTU-AICTE Sponsored STTP on "Role of Libraries and Librarians in Pandemic Era" on 21-02-2022 to 26-02-2022.</li>
+                <li>Organized One Day Workshop on "Internet of Things" for students on 05-02-2022.</li>
+                <li>Organized Seminar on "Fundamentals of Options and Introduction to Trading Strategies" for MBA students on 23-12-2021</li>
+                <li>Organized Seminar on "EIC Analysis of Stock Seminar" for MBA students on 21-12-2021.</li>
+                <li>Organized Seminar on "Statistical Methods for Continuous Data" for MBA students on 20-12-2021.</li>
+                <li>Organized Workshop on "3D- Printing" for Mechanical Engineering students on 02-12-2021.</li>
+                <li>Organized Expert Lecture on "Database Trends and Application" for students on 16-10-2021.</li>
+                <li>Organized "Robot Making Awareness" Program for UG students on 01-09-2021.</li>
+                <li>Organized a Competition on "CAD Cracker (CAD software – Solid works)" for UG ME students on 23-08-2021.</li>
+                <li>Organized Expert Lecture on "Amazon Web Service" for students on 21-08-2021.</li>
+                <li>Organized "Hand on Solid works Software" for UG ME students from 09-08-2021 to 13-08-2021.</li>
+                <li>Organized Webinar on “Brand Management” for MBA Students on 16-07-2021.</li>
+                <li>Organized Webinar on “Computer Vision” for UG IT and CE Sem-7 students on 03-07-2021.</li>
+                <li>Did the Collabration with Phalge Industry for the Internship of students on 29-06-2021.</li>
+                <li>Organized Webinar on "Questionnaire and form design for effective survey" for PG Sem 2 MBA students 28-06-2021.</li>
+                <li>Organized Webinar on "Business Plan Preparations" for PG Sem 2 MBA students on 24-06-2021.</li>
+                <li>Organized Webinar on “Cyber Security” for UG IT and CE Sem-5 students on 23-06-2021.</li>
+                <li>Organized Webinar on "Valuation of Stocks and Bonds" for PG Sem 2 MBA students on 18-06-2021.</li>
+                <li>Did the Tie-up with iACE for Internship and Professional Training of students (MOU) on 13-05-2021.</li>
+                <li>Organized AICTE sponsored webinar on “National Education Policy 2020” for Faculty members on 3-04-2021.</li>
+                <li>Organized GTU sponsored FDP on “Post Lockdown trends in CAD/CAM & Manufacturing for MSME” from 18-01-2021 to 23-01-2021.</li>
+                <li>Organized AICTE sponsored STTP on “Communication Skills and Ethics: A Modern Education Tool in 21st Century” from 18-01-2021 to 23-01-2021.</li>
+                <li>Organized GUJCOST sponsored Two Days Webinar on “Innovation in Robotics & Automation and Its Application” from 04-12-2020 to 05-12-2020.</li>
+                <li>Organized GUJCOST sponsored Two Days State Level Webinar on “Electric Vehicles Modelling & Future Adoption” from 25-11-2020 to 26-11-2020.</li>
+                <li>Organized GUJCOST sponsored Two Days Webinar on “Innovation in Concrete Construction” from 06-11-2020 to 07-11-2020.</li>
+                <li>Organized GUJCOST sponsored Two Days Webinar on “Demystifying Data Science using Machine Learning” from 27-10-2020 to 28-10-2020.</li>
+                <li>Organized GUJCOST sponsored Two Days Webinar “Hands on Machine Learning using R” from 22-10-2020 to 23-10-20.</li>
+                <li>Organized Webinar has been organized on “How to write research paper and how to fetch R&D funding grants” on 18-06-2020.</li>
+                <li>Received AICTE-INAE student travel grant of 1 lac INR for publishing paper in “International Conference on Science Technology and Management” at Abu Dhabi UAE on 27-04-2020.</li>
+                <li>Organized GUJCOST sponsored “International Conference on Data Science and Intelligent Application (ICDSIA-20) from 24-01-2020 to 25-01-20.</li>
+                <li>Organized Two Days STTP on “Strategic Planning and Management for NBA Accreditation.” From 26-12-2019 to 27-12-2019.</li>
+                <li>Organized GUJCOST sponsored Five-Days STTP on “IoT using ARM Cortex M4” from 19-08-2019 to 23-08-2019.</li>
+                <li>Organized GUJCOST sponsored Two days state level seminar on “Mathematical Modelling and Applications of Mathematics in Engineering – 2019 (MMAME-2019)” from 19-07-2019 to 20-07-2019.</li>
               </ul>
             </div>
           </div>
--- gu/src/pages/nisp/nisp.jsx
+++ gu/src/pages/nisp/nisp.jsx
@@ -7,7 +7,72 @@ const Nisp = () => { // Corrected component name to start with an uppercase lett
   return (
     <React.Fragment>
        <Innerhero backgroundImage={inner3} title="National innovation & start-up policy" />
-      <h1>Nisp</h1> {/* Corrected content to uppercase */}
+
+       <div className="content-container">
+        <div className="container container-2">
+          <div className="row">
+            <div className="col-lg-7 mx-auto">
+              <div className="iqac-head text-center">
+                <div>
+                  <h4>National innovation and start-up policy (NISP)</h4>
+                  <p>Gandhinagar Univarsity has initiated the efforts for implementing the National innovation and start-up policy (NISP), under the guidance of the Ministry of Education’s Innovation Cell (MIC). The Motive of this policy is to encourage innovation and start-up environment in the campus, among the students and faculty.</p>
+                </div>
+
+                <div className="iqac-btn-wrap"><a href="#!" className="btn btn-gold-outline">NISP Policy</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content-container pt-0 text-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-9 mx-auto">
+              <h4>Objectives</h4>
+              <ul>
+                <li>mproving creativity, creative thinking and design among faculty, staff and the student community.</li>
+                <li>Developing and assisting groups of students to develop some form of new ideas.</li>
+                <li>Provide pre-incubation incubation and incubation site for the academic / student / staff / student.</li>
+                <li>Organizing webinars, FDP, conferences and workshops for students, faculty, and alumni graduates, and promoting new cultures and entrepreneurs at the institution.</li>
+                <li>Strengthen the environmental and business value of the institution by providing an awareness and protection center for IPR, an industrial communications platform.</li>
+                <li>Generation and postgraduate initiatives at educational institutions that can make a difference in society through their expertise.</li>
+                <li>Compliance with the proposed NISP framework and meeting the key performance indicators set by the MIC.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content-container pt-0 text-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-9 mx-auto">
+              <h4>Functions of IICs</h4>
+              <ul>
+                <li>Establish the status of the Incubation Center and infrastructure to facilitate I&E activities at the center and NER.</li>
+                <li>Provide a platform for skills, students, and alumni to develop new products that are internationally recognized and generate business opportunities.</li>
+                <li>Strategic integration with DST, CII, MSME and other premium institutions for me to teach and collaborate on creating a positive I&E job environment.</li>
+                <li>Improving the quality of research work among I&E-related students and obtaining an IPR / patent for technology transfer of commercial technology.</li>
+                <li>Make the incubation infrastructure self-sufficient and self-sufficient.</li>
+      
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div className="content-container text-container light-bg-text-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-9 mx-auto">
+              <div className="heading text-center">
+                <h4>NISP Committee</h4>
+              </div>
+
+            
+            </div>
+          </div>
+        </div>
+      </div>
+
     </React.Fragment>
   );
 };
