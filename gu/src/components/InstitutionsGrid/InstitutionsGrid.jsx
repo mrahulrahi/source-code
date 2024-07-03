e9dc98de5ab18e5ce263c6b623dc0a39ecbf5632
mrahulrahi <mrahulrahi@gmail.com>
2024-07-04 00:12:36 +0530

    Institutions Grid done

---
+++ gu/src/components/InstitutionsGrid/InstitutionsGrid.jsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import './InstitutionsGrid.css'
+import institutionsImg from '../../assets/images/institutions-grid-img.jpg'
+import institutionsIcon from '../../assets/images/institutions-grid-icon.svg'
+
+
+const InstitutionsGrid = () => {
+    return (
+        <div className="institutions-grid">
+            <div className="institutions-item col-span-3">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-img relative"><img src={institutionsImg} alt="" /></div>
+                    <div className="institutions-title">OUR INSTITUTIONS</div>
+
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Technology</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item row-span-2">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Skill Development</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item row-span-2">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Liberal Studies</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Management</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Science</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Commerce</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Computer Scie....</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>LAW</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Valuation Studies</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Research & Devel....</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Pharmacy</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="institutions-item col-span-2">
+                <div className="institutions-box w-full h-full relative">
+                    <div className="institutions-inner d-flex flex-column h-100">
+                        <div className="institutions-icon"><img src={institutionsIcon} alt="" /></div>
+                        <div className="institutions-content mt-auto">
+                            <h6>Design</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Nibh dignissim sagittis ornare et duis blandit mattis.....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default InstitutionsGrid
--- gu/src/pages/home/home.jsx
+++ gu/src/pages/home/home.jsx
@@ -4,29 +4,32 @@ import Innerhero from '../../components/innerhero/innerhero';
 import inner1 from '../../assets/images/inner-img-1.jpg';
 import FacilityGrid from '../../components/FacilityGrid/FacilityGrid';
 import Specializations from '../../components/Specializations/specializations';
-import Heading from '../../components/heading/heading'; 
+import Heading from '../../components/heading/heading';
 import InfoCompany from '../../components/InfoCompany/InfoCompany';
+import InstitutionsGrid from '../../components/InstitutionsGrid/InstitutionsGrid';
 
 
 const home = () => {
   return (
     <React.Fragment>
 
-        <Innerhero backgroundImage={inner1} title="Institution’s Innovation Council" />
-        <h1> Home </h1>
+      <Innerhero backgroundImage={inner1} title="Institution’s Innovation Council" />
+      <h1> Home </h1>
+      
+      <InstitutionsGrid />
 
-        <div className="content-container p-0 bg-white">
-          <Specializations />
-        </div>
+      <div className="content-container p-0 bg-white">
+        <Specializations />
+      </div>
 
-        <div className="content-container p-0 bg-white">
-            <Heading sheading="OUR" shheading="FACILITIES" spara=" The campus provides a refreshing environment with <br /> different facilities which makes our students feel at home." />
-            <FacilityGrid />
-        </div>
-        <div className="content-container blue-linear">
-            <InfoCompany />   
-        </div>
-        
+      <div className="content-container p-0 bg-white">
+        <Heading sheading="OUR" shheading="FACILITIES" spara=" The campus provides a refreshing environment with <br /> different facilities which makes our students feel at home." />
+        <FacilityGrid />
+      </div>
+      <div className="content-container blue-linear">
+        <InfoCompany />
+      </div>
+
 
     </React.Fragment>
   )
