b15cbc3e90c6ebb797221ccf19da6c887ee95a31
mrahulrahi <mrahulrahi@gmail.com>
2024-11-15 01:33:00 +0530

    images added and changes done

--- crypto-key/include/js/custom.js
+++ crypto-key/include/js/custom.js
@@ -3,12 +3,6 @@ var $ = jQuery.noConflict();
 jQuery(document).ready(function ($) {
 
   /*==========================*/
-  /* sliders */
-  /*==========================*/
-
-
-
-  /*==========================*/
   /* Theme Switcher */
   /*==========================*/
   $('#flexSwitchCheckDefault').change(function () {
@@ -18,34 +12,6 @@ jQuery(document).ready(function ($) {
       $("body").removeClass("dark-theme");
     }
   });
-
-
-
-  /*==========================*/
-  /* Scroll on animate */
-  /*==========================*/
-  function onScrollInit(items, trigger) {
-    items.each(function () {
-      var osElement = $(this),
-        osAnimationClass = osElement.attr('data-os-animation'),
-        osAnimationDelay = osElement.attr('data-os-animation-delay');
-      osElement.css({
-        '-webkit-animation-delay': osAnimationDelay,
-        '-moz-animation-delay': osAnimationDelay,
-        'animation-delay': osAnimationDelay
-      });
-      var osTrigger = (trigger) ? trigger : osElement;
-      osTrigger.waypoint(function () {
-        osElement.addClass('animated').addClass(osAnimationClass);
-      }, {
-        triggerOnce: true,
-        offset: '95%',
-      });
-      // osElement.removeClass('fadeInUp');
-    });
-  }
-  onScrollInit($('.os-animation'));
-  onScrollInit($('.staggered-animation'), $('.staggered-animation-container'));
 
 
   /*==========================*/
