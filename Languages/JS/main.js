d4fef761af3332462503a4b3103b6edeb99fd9c2
mrahulrahi <mrahulrahi@gmail.com>
2025-01-17 00:20:13 +0530

    practice js

--- Languages/JS/main.js
+++ Languages/JS/main.js
@@ -3,3 +3,16 @@ function fullName(first, last) {
 }
 
 fullName('Rahul', 'Maurya')
+
+let num = 5;
+var age;
+let name;
+
+let message = "Hello World";
+let _message = "Hello World";
+let $message = "Hello World";
+
+num =10;
+
+console.log(num);
+console.log(age);
