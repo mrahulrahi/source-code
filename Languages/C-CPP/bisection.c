36b7fda0bdf4ea827af90b1604457b8aac83c6fa
mrahulrahi <mrahulrahi@gmail.com>
2025-02-05 23:52:18 +0530

    more added

--- Languages/C-CPP/bisection.c
+++ Languages/C-CPP/bisection.c
@@ -27,3 +27,42 @@ int main() {
     bisection(a, b, tol);
     return 0;
 }
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double a[3][4], x, y, z;
+
+    // Input the coefficients and constants
+    cout << "Enter the coefficients and constants:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    // Perform Gauss-Elimination
+    for (int i = 0; i < 2; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            double ratio = a[j][i] / a[i][i];
+            for (int k = 0; k < 4; k++) {
+                a[j][k] = a[j][k] - ratio * a[i][k];
+            }
+        }
+    }
+
+    // Back-substitution
+    z = a[2][3] / a[2][2];
+    y = (a[1][3] - a[1][2] * z) / a[1][1];
+    x = (a[0][3] - a[0][1] * y - a[0][2] * z) / a[0][0];
+
+    cout << "\nSolution:\n";
+    cout << "x = " << fixed << setprecision(2) << x << "\n";
+    cout << "y = " << fixed << setprecision(2) << y << "\n";
+    cout << "z = " << fixed << setprecision(2) << z << "\n";
+
+    return 0;
+}
